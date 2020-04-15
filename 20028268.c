50038976d8b5540279fc708427ec0f6c51f3dd54
talentam <1822024838@qq.com>
2020-04-15 14:38:58 +0800

    upload the right file

--- 20028268.c
+++ 20028268.c
@@ -1,5 +1,432 @@
-#include <stdio.h>
+//
+//  main.c
+//  mknapsack
+//
+//  Created by Bai on 14/03/2020.
+//  Copyright © 2019 UNNC. All rights reserved.
+//
 
-int main(){
-    printf("hi\n");
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/* global parameters */
+int RAND_SEED[] = {1,20,30,40,50,60,70,80,90,100,110, 120, 130, 140, 150, 160, 170, 180, 190, 200};
+int NUM_OF_RUNS = 5;
+int MAX_TIME = 30;  //max amount of time permited (in sec)
+int num_of_problems;    //number of problems
+
+
+/* parameters for evlutionary algorithms*/
+static int POP_SIZE = 50;   //please modify these parameters according to your problem
+int MAX_NUM_OF_GEN = 10000; //max number of generations
+float CROSSOVER_RATE = 0.8;
+float MUTATION_RATE = 0.1;
+
+/* declare parameters for simulated annealing here */
+
+
+/* declare parameteres for tabu search here*/
+
+
+/* declare parameters for variable neighbourhood search here*/
+
+
+struct solution_struct best_sln;  //global best solution
+
+//return a random number between 0 and 1
+float rand_01()
+{
+    float number;
+    number = (float) rand();
+    number = number/RAND_MAX;
+    //printf("rand01=%f\n", number);
+    return number;
+}
+
+//return a random nunber ranging from min to max (inclusive)
+int rand_int(int min, int max)
+{
+    int div = max-min+1;
+    int val =rand() % div + min;
+    //printf("rand_range= %d \n", val);
+    return val;
+}
+
+
+struct item_struct{
+    int dim; //no. of dimensions
+    int* size; //volume of item in all dimensions
+    int p;
+};
+
+struct problem_struct{
+    int n; //number of items
+    int dim; //number of dimensions
+    struct item_struct* items;
+    int* capacities;  //knapsack capacities
+};
+
+void free_problem(struct problem_struct* prob)
+{
+    if(prob!=NULL)
+    {
+        if(prob->capacities !=NULL) free(prob->capacities);
+        if(prob->items!=NULL)
+        {
+            for(int j=0; j<prob->n; j++)
+            {
+                if(prob->items[j].size != NULL)
+                    free(prob->items[j].size);
+            }
+            free(prob->items);
+        }
+        free(prob);
+    }
+}
+
+void init_problem(int n, int dim, struct problem_struct** my_prob)
+{
+    struct problem_struct* new_prob = malloc(sizeof(struct problem_struct));
+    new_prob->n=n; new_prob->dim=dim;
+    new_prob->items=malloc(sizeof(struct item_struct)*n);
+    for(int j=0; j<n; j++)
+        new_prob->items[j].size= malloc(sizeof(int)*dim);
+    new_prob->capacities = malloc(sizeof(int)*dim);
+    *my_prob = new_prob;
+}
+
+
+//example to create problem instances, actual date should come from file
+struct problem_struct** load_problems(char* data_file)
+{
+    int i,j,k;
+    //int num_of_probs;
+    FILE* pfile = fopen(data_file, "r");
+    if(pfile==NULL)
+        {printf("Data file %s does not exist. Please check!\n", data_file); exit(2); }
+    fscanf (pfile, "%d", &num_of_problems);
+ 
+    struct problem_struct** my_problems = malloc(sizeof(struct problem_struct*)*num_of_problems);
+    for(k=0; k<num_of_problems; k++)
+    {
+        int n, dim, obj_opt;
+        fscanf (pfile, "%d", &n);       // number of items
+        fscanf (pfile, "%d", &dim);     // nmuber of dimensions
+        fscanf (pfile, "%d", &obj_opt); // optimal solution value (zero if unavailable)
+        
+        init_problem(n, dim, &my_problems[k]);  //allocate data memory
+        for(j=0; j<n; j++)
+        {
+            my_problems[k]->items[j].dim=dim;
+            fscanf(pfile, "%d", &(my_problems[k]->items[j].p)); //read profit data
+            //printf("item[j].p=%d\n",my_problems[k]->items[j].p);
+        }
+        for(i=0; i<dim; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                fscanf(pfile, "%d", &(my_problems[k]->items[j].size[i])); //read size data
+                //printf("my_problems[%i]->items[%i].size[%i]=%d\n",k,j,i,my_problems[k]->items[j].size[i]);
+            }
+        }
+        for(i=0; i<dim; i++){
+            fscanf(pfile, "%d", &(my_problems[k]->capacities[i]));
+            //printf("capacities[i]=%d\n",my_problems[k]->capacities[i] );
+        }
+    }
+    fclose(pfile); //close file
+    return my_problems;
+}
+
+struct solution_struct{
+    struct problem_struct* prob; //maintain a shallow copy of the problem data
+    float objective;    //the price of the current solution
+    int feasibility; //indicate the feasiblity of the solution, -i means capacity overload in i dimension
+    int* x; //chromosome vector
+    int* cap_left; //capacity left in all dimensions
+};
+
+void free_solution(struct solution_struct* sln)
+{
+    if(sln!=NULL)
+    {
+        free(sln->x);
+        free(sln->cap_left);
+        sln->objective=0;
+        sln->prob=NULL;
+        sln->feasibility=false;
+    }
+}
+
+//copy a solution from another solution
+bool copy_solution(struct solution_struct* dest_sln, struct solution_struct* source_sln)
+{
+    if(source_sln ==NULL) return false;
+    if(dest_sln==NULL)
+    {
+        dest_sln = malloc(sizeof(struct solution_struct));
+    }
+    else{
+        free(dest_sln->cap_left);
+        free(dest_sln->x);
+    }
+    int n = source_sln->prob->n;
+    int m =source_sln->prob->dim;
+    dest_sln->x = malloc(sizeof(int)*n);
+    dest_sln->cap_left=malloc(sizeof(int)*m);
+    for(int i=0; i<m; i++)
+        dest_sln->cap_left[i]= source_sln->cap_left[i];
+    for(int j=0; j<n; j++)
+        dest_sln->x[j] = source_sln->x[j];
+    dest_sln->prob= source_sln->prob;
+    dest_sln->feasibility=source_sln->feasibility;
+    dest_sln->objective=source_sln->objective;
+    return true;
+}
+
+
+
+void evaluate_solution(struct solution_struct* sln)
+{
+    //evaluate the feasibility and objective of the solution
+    sln->objective =0; sln->feasibility = 1;
+    struct item_struct* items_p = sln->prob->items;
+    
+    for(int i=0; i< items_p->dim; i++)
+    {
+        sln->cap_left[i]=sln->prob->capacities[i];
+        for(int j=0; j<sln->prob->n; j++)
+        {
+            sln->cap_left[i] -= items_p[j].size[i]*sln->x[j];
+            if(sln->cap_left[i]<0) {
+                sln->feasibility = -1*i; //exceeding capacity
+                return;
+            }
+        }
+    }
+    if(sln->feasibility>0)
+    {
+        for(int j=0; j<sln->prob->n; j++)
+        {
+            sln->objective += sln->x[j] * items_p[j].p;
+        }
+    }
+}
+
+//output a given solution to a file
+void output_solution(struct solution_struct* sln, char* out_file)
+{
+    if(out_file !=NULL){
+        FILE* pfile = fopen(out_file, "a"); //append solution data
+        fprintf(pfile, "%i\n", (int)sln->objective);
+        for(int i=0; i<sln->prob->n; i++)
+        {
+            fprintf(pfile, "%i ", sln->x[i]);
+        }
+        fprintf(pfile, "\n");
+        /*for(int j=0; j<sln->prob->n; j++)
+            fprintf(pfile, "%i ", sln->prob->items[j].p);
+        fprintf(pfile, "\n");*/
+        fclose(pfile);
+    }
+    else
+        printf("sln.feas=%d, sln.obj=%f\n", sln->feasibility, sln->objective);
+}
+
+
+//check the  feasiblity and obj values of solutons from solution_file.
+//return 0 is all correct or the index of the first infeasible problem [1, num_of_problems].
+int check_solutions(struct problem_struct** my_problems, char* solution_file)
+{
+    FILE * pfile= fopen(solution_file, "r");
+    if(pfile==NULL)
+    {
+        printf("Solution file %s does not exist. Please check!\n", solution_file);
+        exit(2);
+    }
+    float val_obj;
+    int val;
+    fscanf (pfile, "%i", &val);
+    if(val != num_of_problems)
+    {
+        printf("The stated number of solutions does not match the number of problems.\n");
+        exit(3);
+    }
+    struct solution_struct temp_sln;
+    int count=0, k=0;
+    int n, dim;
+    while(fscanf (pfile, "%f", &val_obj)!=EOF && k<num_of_problems)
+    {
+        //val_obj = val;
+        n= my_problems[k]->n;  dim= my_problems[k]->dim;
+        temp_sln.x = malloc(sizeof(int)*n);
+        temp_sln.cap_left=malloc(sizeof(int)*dim);
+        temp_sln.prob = my_problems[k];
+        while(fscanf (pfile, "%i", &val)!=EOF)
+        {
+            if(val<0 || val>1) {fclose(pfile);  return k+1;} //illeagal values
+            temp_sln.x[count] = val;
+            count++;
+            if(count==n)
+            {
+                evaluate_solution(&temp_sln);
+                if(!temp_sln.feasibility || fabs(temp_sln.objective - val_obj)>0.01)
+                {
+                    fclose(pfile);
+                    //printf("feasb=%i, obj= %f, val=%i\n",temp_sln.feasibility, temp_sln.objective, val_obj);
+                    //output_solution(&temp_sln, "my_debug.txt");
+                    return k+1;  //infeasible soltuion or wrong obj
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        count=0; k++;
+        
+        free(temp_sln.x); free(temp_sln.cap_left);
+    }
+    fclose(pfile);
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////
+
+
+//intialise the population with random solutions
+void init_population(struct problem_struct* prob, struct solution_struct* pop){
+    //todo
+
+}
+
+void cross_over(struct solution_struct* pop)
+{
+    //todo
+}
+
+void mutation(struct solution_struct* pop)
+{
+    //todo
+}
+////////////////////////////////////////////////////////////////////////////
+
+//modify the solutions that violate the capacity constraints
+void feasibility_repair(struct solution_struct* pop)
+{
+    //todo
+}
+
+//local search
+void local_search_first_descent(struct solution_struct* pop)
+{
+    //todo
+}
+
+void replacement(struct solution_struct* curt_pop, struct solution_struct* new_pop){
+    //todo
+}
+
+//update global best solution from sln
+void update_best_solution(struct solution_struct* sln)
+{
+    if(best_sln.objective < sln->objective)
+    copy_solution(&best_sln, sln);
+}
+
+//memetic algorithm
+int memeticAlgorithm(struct problem_struct* prob)
+{
+    
+    clock_t time_start, time_fin;
+    time_start = clock();
+    double time_spent=0;
+    int iter =0;        //number of generation
+    /////////////////
+    struct solution_struct parent_pop[POP_SIZE];
+    struct solution_struct offspring_pop[POP_SIZE];
+    init_population(prob, parent_pop);
+    init_population(prob, offspring_pop);
+    /////////////////
+
+    while(iter<MAX_NUM_OF_GEN && time_spent < MAX_TIME)
+    {
+        //////////////////////
+        //take all individuals to crossover
+        cross_over(offspring_pop);
+        mutation(offspring_pop);
+        feasibility_repair(offspring_pop);
+        local_search_first_descent(offspring_pop);
+        replacement(parent_pop, offspring_pop);
+        ///////////////////////
+        iter++;
+        time_fin=clock();
+        time_spent = (double)(time_fin-time_start)/CLOCKS_PER_SEC;
+    }
+    update_best_solution(parent_pop);
+    
+    //output_solution(&best_sln[0], "my_debug.txt");
+    
+    return 0;
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    printf("Starting the run!\n");
+    char data_file[50]={"somefile"}, out_file[50]={}, solution_file[50]={};  //max 50 problem instances per run
+    if(argc<3)
+    {
+        printf("Insufficient arguments. Please use the following options:\n   -s data_file (compulsory)\n   -o out_file (default my_solutions.txt)\n   -c solution_file_to_check\n   -t max_time (in sec)\n");
+        return 1;
+    }
+    else if(argc>9)
+    {
+        printf("Too many arguments.\n");
+        return 2;
+    }
+    else
+    {
+        for(int i=1; i<argc; i=i+2)
+        {
+            if(strcmp(argv[i],"-s")==0)
+                strcpy(data_file, argv[i+1]);
+            else if(strcmp(argv[i],"-o")==0)
+                strcpy(out_file, argv[i+1]);
+            else if(strcmp(argv[i],"-c")==0)
+                strcpy(solution_file, argv[i+1]);
+            else if(strcmp(argv[i],"-t")==0)
+                MAX_TIME = atoi(argv[i+1]);
+        }
+        //printf("data_file= %s, output_file= %s, sln_file=%s, max_time=%d", data_file, out_file, solution_file, MAX_TIME);
+    }
+    struct problem_struct** my_problems = load_problems(data_file);
+    
+    if(strlen(solution_file)<=0)
+    {
+        if(strcmp(out_file,"")==0) strcpy(out_file, "my_solutions.txt"); //default output
+        FILE* pfile = fopen(out_file, "w"); //open a new file
+        fprintf(pfile, "%d\n", num_of_problems); fclose(pfile);
+        for(int k=0; k<num_of_problems; k++)
+        {
+            best_sln.objective=0; best_sln.feasibility=0;
+            for(int run=0; run<NUM_OF_RUNS; run++)
+            {
+                srand(RAND_SEED[run]);
+                
+                memeticAlgorithm(my_problems[k]); // call SA method
+            }
+            output_solution(&best_sln,out_file);
+        }
+    }
+    for(int k=0; k<num_of_problems; k++)
+    {
+       free_problem(my_problems[k]); //free problem data memory
+    }
+    free(my_problems); //free problems array
+    if(best_sln.x!=NULL && best_sln.cap_left!=NULL){ free(best_sln.cap_left); free(best_sln.x);} //free global
+    return 0;
 }
